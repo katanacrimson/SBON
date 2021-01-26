b5ae1eed3c5258a4a7836d3532997bc033fe5329
Damian Bushong <katana@odios.us>
2021-01-26 16:48:26 -0600

    update docs and version

---
+++ build/index.d.ts
@@ -0,0 +1,1 @@
+export { SBON } from './SBON';
---
+++ build/index.js
@@ -0,0 +1,14 @@
+"use strict";
+//
+// SBON - JS library for working with SBON binary format.
+//
+// @copyright (c) 2018 Damian Bushong <katana@odios.us>
+// @license MIT license
+// @url <https://github.com/damianb/SBON>
+//
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SBON = void 0;
+// istanbul ignore file
+var SBON_1 = require("./SBON");
+Object.defineProperty(exports, "SBON", { enumerable: true, get: function () { return SBON_1.SBON; } });
+//# sourceMappingURL=index.js.map
